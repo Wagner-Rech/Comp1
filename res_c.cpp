0bcd256937311cda39bf112c11c55942ec2c6271
Wagner-Rech <wagner.neto@estudante.uffs.edu.br>
2025-05-19 10:56:11 -0300

    Work In Progress

---
+++ res_c.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int total,convidados;
+	cin >> total >> convidados;
+	int hierarquia[n+1];
+	vector<set<int>> chefe(n);
+	for (int i = 2; i <= n; i++) {
+		cin >> chefe(i).insert();
+	}
+	return 0;
+}
